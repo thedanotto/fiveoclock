453a7d80a577a115cc073a63a5fc7aa4e1444dce
Dan Otto <theDanOtto@gmail.com>
2014-02-06 19:58:08 -0600

    static serving

--- fiveoclock/urls.py
+++ fiveoclock/urls.py
@@ -6,6 +6,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'city.views.home', name='home'),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT }),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT }),
     # url(r'^blog/', include('blog.urls')),
     
     url(r'^admin/', include(admin.site.urls)),
